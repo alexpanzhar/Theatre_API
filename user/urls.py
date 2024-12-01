a3d2d39ae8be6efbf9a3c85c9e20c132f9b98ebe
alexpanzhar <alex.panzhar1@gmail.com>
2024-12-01 15:38:12 +0100

    feat: add user registration and profile management functionality
    - Created `UserSerializer` for user creation and updates with encrypted password handling.
    - Added `CreateUserView` for user registration:
      - Allows new users to register without authentication.
    - Added `ManageUserView` for authenticated user profile management:
      - Supports retrieving and updating the current user's details.
      - Integrated JWT authentication for secure access.
    - Updated `user/urls.py`:
      - Added endpoints `/register/` for registration and `/me/` for profile management.

--- user/urls.py
+++ user/urls.py
@@ -5,9 +5,13 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView,
 )
 
+from user.views import CreateUserView, ManageUserView
+
 app_name = "user"
 urlpatterns = [
+    path("register/", CreateUserView.as_view(), name="create"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("me/", ManageUserView.as_view(), name="manage"),
 ]
