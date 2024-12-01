1c548430e755f9eba525c63ae045273b296c8d09
alexpanzhar <alex.panzhar1@gmail.com>
2024-12-01 14:37:33 +0100

    feat: implement custom permission and configure default permissions
    - Added `IsAdminOrIfAuthenticatedReadOnly` to `user/permissions.py`:
      - Allows read-only access for authenticated users.
      - Grants full access to admin users.
    - Updated `DEFAULT_PERMISSION_CLASSES` in `settings.py`:
      - Set `IsAdminOrIfAuthenticatedReadOnly` as the default permission class.

--- Theatre_API/settings.py
+++ Theatre_API/settings.py
@@ -135,7 +135,10 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    )
+    ),
+    "DEFAULT_PERMISSION_CLASSES": [
+        "user.permissions.IsAdminOrIfAuthenticatedReadOnly",
+    ],
 }
 
 SIMPLE_JWT = {
