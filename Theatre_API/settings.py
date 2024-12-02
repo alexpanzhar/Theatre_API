e450c4c8b4ac04b7065ecb013cda2605159fdd04
alexpanzhar <alex.panzhar1@gmail.com>
2024-12-02 14:01:31 +0100

    feat: configure API rate throttling
    - Added `DEFAULT_THROTTLE_CLASSES` to settings.py:
      - `AnonRateThrottle` for anonymous users.
      - `UserRateThrottle` for authenticated users.
    - Defined `DEFAULT_THROTTLE_RATES`:
      - Anonymous users: 100 requests/day.
      - Authenticated users: 1000 requests/day.

--- Theatre_API/settings.py
+++ Theatre_API/settings.py
@@ -140,7 +140,15 @@ REST_FRAMEWORK = {
         "user.permissions.IsAdminOrIfAuthenticatedReadOnly",
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
-    "PAGE_SIZE": 25
+    "PAGE_SIZE": 25,
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle"
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "100/day",
+        "user": "1000/day"
+    }
 }
 
 SIMPLE_JWT = {
