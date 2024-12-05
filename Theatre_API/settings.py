a0aea9cf1971d7d2466d563a758f0adbaae5a18e
alexpanzhar <alex.panzhar1@gmail.com>
2024-12-05 17:29:16 +0100

    feat: dockerize the application
    - Added `Dockerfile` for building the application image.
    - Configured `docker-compose.yml` to set up containers for the app, database, and static files.
    - Integrated environment variables for database credentials and settings.
    - Automated database migrations and static files handling within Docker containers.
    - Enabled easy deployment and testing via Docker.

--- Theatre_API/settings.py
+++ Theatre_API/settings.py
@@ -9,9 +9,12 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from datetime import timedelta
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-fr%4dnsac*li@31az=e$*-zvdfu*zgpub2e6%=2m9(q2@6%itb"
-)
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ["DEBUG"] == "True"
 
 INTERNAL_IPS = [
     # ...
@@ -88,8 +89,12 @@ WSGI_APPLICATION = "Theatre_API.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["POSTGRES_DB"],
+        "USER": os.environ["POSTGRES_USER"],
+        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
+        "HOST": os.environ["POSTGRES_HOST"],
+        "PORT": os.environ["POSTGRES_PORT"],
     }
 }
 
@@ -128,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = BASE_DIR / "files/media"
 MEDIA_URL = "/media/"
 
 # Default primary key field type
